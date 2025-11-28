b888c02fe43039b08b128d21c76dc8de337c5d03
hdsjiw <bisha0821@gmail.com>
2025-11-28 14:25:16 +0900

    init: project initial setup

---
+++ backend/astro/src/test/java/com/astro/astro/AstroBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.astro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
